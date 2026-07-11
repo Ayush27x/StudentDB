a9b05f11da786de78cd0350937df7ecd9247640c
DEMO STUDENT <djayush2004@gmail.com>
2026-07-11 16:22:18 +0530

    all set

--- edit.php
+++ edit.php
@@ -20,28 +20,19 @@ $id = $_GET['id'];
 // ======================================
 
 $sql = "SELECT * FROM studentdb WHERE id='$id'";
-
 $result = mysqli_query($con, $sql);
-
 $row = mysqli_fetch_assoc($result);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Edit Student</title>
-
     <link rel="stylesheet" href="style_edit.css">
-
 </head>
-
 <body>
 
 <div class="container">
